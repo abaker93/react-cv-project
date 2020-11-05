016f391d23e56bb004b39acd6cd3ddec03d976c4
abaker93 <anna.baker.design@gmail.com>
2020-11-05 12:37:20 -0500

    file setup and basic styling

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+    render() {
+        return (
+            <div id="Form">
+                <h2>Basic Information</h2>
+            </div>
+        )
+    }
+}
+
+export default Form;
