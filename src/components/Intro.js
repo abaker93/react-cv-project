56178692b70a9ac810e20594f9c8f04454ff7667
abaker93 <anna.baker.design@gmail.com>
2020-11-10 22:36:33 -0500

    editable intro section

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,24 +1,73 @@
 import React, { Component } from "react";
 
 class Intro extends Component {
+    constructor() {
+        super();
 
-    changePhoto = e => {
-        
+        this.state = {
+            name: '',
+            title: '',
+            profile: '',
+            edit: false
+        };;
     }
-    
+
+    handleNameChange = e => {
+        this.setState({
+            name: e.target.value,
+        })
+    }
+
+    handleTitleChange = e => {
+        this.setState({
+            title: e.target.value,
+        })
+    }
+
+    handleProfileChange = e => {
+        this.setState({
+            profile: e.target.value,
+        })
+    }
+
+    toggleEdit = () => {
+        this.setState(prevState => ({
+            edit: !prevState.edit
+        }))
+    }
+
     render() {
+        const { name, title, profile } = this.state;
+
         return (
             <div id="Intro">
-                <h1>First Last Name</h1>
-                <p class="title">Web Developer</p>
+                <h1>{name ? name : 'Your name'}</h1>
+                <p class="title">{title ? title : 'Your title'}</p>
                 <h2>Profile</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p class="profile">{profile ? profile : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}</p>
+                <form id="intro-edit" class="edit-form">
+                    <label for="name">Your name</label>
+                    <input
+                        id="name"
+                        type="text"
+                        onChange={this.handleNameChange}
+                    />
+                    <label for="title">Your title</label>
+                    <input
+                        id="title"
+                        type="text"
+                        onChange={this.handleTitleChange}
+                    />
+                    <label for="profile">Profile</label>
+                    <textarea
+                        id="profile"
+                        onChange={this.handleProfileChange}
+                    />
+                    <button type="button" onClick={this.toggleEdit}>Close Edit</button>
+                </form>
             </div>
-
-            
         )
     }
-
 }
 
 export default Intro;
